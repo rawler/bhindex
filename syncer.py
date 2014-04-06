530727d1750b6a3343d47a3041bd0e47d017550b
Ulrik Mikaelsson <ulrik.mikaelsson@gmail.com>
2014-04-06 21:35:03 +0200

    [syncer]Improved connector, avoid double-direction connections, and avoid reconnecting until connection is actually gone.

--- syncer.py
+++ syncer.py
@@ -25,6 +25,7 @@ class SyncConnection(object):
         self._last_serial_received = 0
 
     def handshake(self):
+        assert not self.peername
         self._sendmsg('hello', sync_pb2.Hello(name=self.name))
         peerhello = self.read_msg()
         self.peername = peerhello.name
@@ -45,9 +46,8 @@ class SyncConnection(object):
         if peersetup.last_serial_in_db < self._last_serial_received:
             print "Warning: detecting remote db was reset"
             self._last_serial_received = 0
-            self._db.set_sync_state(self.peername, last_received=self._last_serial_received)
 
-        return True
+        return self
 
     def _sendmsg(self, field, msg):
         enc = encoder.MessageEncoder(FIELD_MAP[field].number, False, False)
@@ -57,11 +57,12 @@ class SyncConnection(object):
                 enc(buf.write, msg)
         else:
             enc(buf.write, msg)
-        self._sock.sendall(buf.getvalue())
+        self._sock and self._sock.sendall(buf.getvalue())
 
     def _fill_buf(self):
-        res = self._sock.recv(512*1024)
+        res = self._sock and self._sock.recv(64*1024)
         if not res:
+            self.close()
             raise StopIteration
         self._buf += res
 
@@ -90,7 +91,9 @@ class SyncConnection(object):
                 self._fill_buf()
 
     def close(self):
-        self._sock.close()
+        if self._sock:
+            self._sock.close()
+            self._sock = None
 
     def run(self):
         last_serial=self._last_serial_received
@@ -105,6 +108,7 @@ class SyncConnection(object):
             self._last_serial_received = last_serial
             self._db.set_sync_state(self.peername, last_received=last_serial)
             self._db.commit()
+            eventlet.sleep()
 
     def db_push(self):
         last_serial = self._last_serial_sent
@@ -116,49 +120,77 @@ class SyncConnection(object):
         self._last_serial_sent = last_serial
 
 class SyncServer(object):
-    def __init__(self, db, name, port, connect, db_poll_interval=0.5):
+    def __init__(self, db, name, port, connectAddresses, db_poll_interval=0.5):
         self._db = db
         self.name = name
         self.port = port
         self.connections = dict()
-        self.connectors = connect
+        self.connectAddresses = connectAddresses
         self._sock = eventlet.listen(('0.0.0.0', port))
         self._server = eventlet.spawn(eventlet.serve, self._sock, self._spawn)
         self._pusher = eventlet.spawn(self._db_push)
         self._connector = eventlet.spawn(self._connector)
 
-    def _spawn(self, sock, client_addr):
-        conn = SyncConnection(self._db, self.name, sock)
-        if conn.handshake():
-            peername = conn.peername
-            print '%s connected from %s' % (peername, client_addr)
+    def _connectPeer(self, conn, connectAddress=None):
+        peername = conn.peername
+        connected = self.connections.get(peername, None)
+        if connected: # Was already connected
+            if connectAddress and not getattr(connected, 'connectAddress', None):
+                connected.connectAddress = connectAddress
+                self.connectAddresses.discard(connectAddress)
+            return None
+        else:
+            conn.connectAddress = connectAddress
+            self.connectAddresses.discard(connectAddress)
             self.connections[peername] = conn
-            try:
-                conn.run()
-            finally:
-                self.connections.pop(peername, None)
+            return conn
+
+    def _disconnectPeer(self, conn):
+        self.connections.pop(conn.peername, None)
+        connectAddress = getattr(conn, 'connectAddress', None)
+        if connectAddress:
+            self.connectAddresses.add(connectAddress)
+
+    def _spawn(self, sock, client_addr, connectAddress=None):
+        conn = SyncConnection(self._db, self.name, sock)
+        try:
+            conn.handshake()
+        except Exception, e:
+            print "Handshake failed: %s" % e
+            return
+
+        peername = conn.peername
+        if client_addr == connectAddress:
+            print 'connected to %s %s' % (peername, client_addr)
+        else:
+            print '%s connected from %s' % (peername, client_addr)
+        if not self._connectPeer(conn, connectAddress):
+            print '%s already connected. Dropping.' % peername
+            return
+
+        try:
+            conn.run()
+        except Exception, e:
+            print "%s died with %s" % (peername, e)
+
+        print '%s disconnected from %s' % (conn.peername, client_addr)
+        self._disconnectPeer(conn)
 
     def _connector(self):
-        def _spawn(sock, addr):
+        def _connect(addr):
+            print addr
             try:
-                self._spawn(sock, addr)
-            finally:
-                self.connectors.add(addr)
+                return addr, eventlet.connect(addr)
+            except Exception, e:
+                print e
+            return addr, None
 
+        connectPool = eventlet.GreenPool(20)
         while True:
-            for addr in set(self.connectors):
-                sock = None
-                try:
-                    print addr
-                    sock = eventlet.connect(addr)
-                except Exception, e:
-                    print e
-                    pass
+            for address, sock in connectPool.imap(_connect, set(self.connectAddresses)):
                 if sock:
-                    self.connectors.remove(addr)
-                    eventlet.spawn(_spawn, sock, addr)
-            eventlet.greenthread.sleep(3)
-
+                    eventlet.spawn(self._spawn, sock, address, connectAddress=address)
+            eventlet.greenthread.sleep(30)
 
     def _db_push(self):
         while True:
@@ -166,8 +198,8 @@ class SyncServer(object):
                 try:
                     conn.db_push()
                 except Exception, e:
+                    print "%s push hit error %s" % (conn.peername, e)
                     conn.close()
-                    self.connections.pop(conn.peername, None)
             eventlet.greenthread.sleep(0.5)
 
     def wait(self):
@@ -192,7 +224,7 @@ sync_config = config.items('LIVESYNC')
 sync_config = {
     'name': sync_config['name'],
     'port': int(sync_config['port']),
-    'connect': set(parse_addr(addr) for addr in sync_config['connect'].split(",")),
+    'connectAddresses': set(parse_addr(addr) for addr in sync_config['connect'].split(",")),
     'db_poll_interval': float(sync_config['db_poll_interval']),
 }
 SyncServer(db=db.open(config), **sync_config).wait()
