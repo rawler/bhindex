ee69e81c95eea36e1ae1059c09a5c0e0dd15e0cd
Ulrik Mikaelsson <ulrik.mikaelsson@gmail.com>
2015-08-02 12:00:06 +0200

    [db/database]PEP-8 formatting

--- db/database.py
+++ db/database.py
@@ -8,30 +8,34 @@ from obj import Object, ValueSet, ANY
 from _setup import create_DB
 
 # Pointers to empty list will be wiped after 30 days.
-DEFAULT_GRACE=3600*24*30
+DEFAULT_GRACE = 3600 * 24 * 30
 
+
+# Matcher-class to match a prefix of a value
 class Starts(unicode):
     pass
 
-def _sql_condition(k,v):
+
+# Generate an SQL condition that finds object with matching criteria
+def _sql_condition(k, v):
     equal_query = """SELECT DISTINCT objid FROM map
-                        NATURAL JOIN list
-                        NATURAL JOIN key
-                     WHERE key = ? AND list.value = ?"""
+                       NATURAL JOIN list
+                       NATURAL JOIN key
+                       WHERE key = ? AND list.value = ?"""
     starts_query = """SELECT DISTINCT objid FROM map
-                     WHERE
-                        keyid = (SELECT keyid FROM key WHERE key = ?)
-                       AND
-                        listid IN (SELECT listid FROM list WHERE value GLOB '%s*')"""
-    any_query =   """SELECT DISTINCT objid FROM map
-                        NATURAL JOIN key
+                        WHERE
+                          keyid = (SELECT keyid FROM key WHERE key = ?)
+                            AND
+                            listid IN (SELECT listid FROM list WHERE value GLOB '%s*')"""
+    any_query = """SELECT DISTINCT objid FROM map
+                     NATURAL JOIN key
                      WHERE key = ?"""
-    absent_query =   """SELECT DISTINCT objid FROM obj
-                            WHERE NOT EXISTS (
-                                SELECT 1 FROM map
-                                    NATURAL JOIN key
-                                    WHERE obj.objid = map.objid AND key = ? AND listid IS NOT NULL
-                            )"""
+    absent_query = """SELECT DISTINCT objid FROM obj
+                        WHERE NOT EXISTS (
+                          SELECT 1 FROM map
+                            NATURAL JOIN key
+                            WHERE obj.objid = map.objid AND key = ? AND listid IS NOT NULL
+                        )"""
     if v is ANY:
         return (any_query, (k,))
     elif v is None:
@@ -41,23 +45,27 @@ def _sql_condition(k,v):
     else:
         return (equal_query, (k, v))
 
+
 def _sql_for_criteria(crit):
     sql = []
     params = []
-    for k,v in crit.iteritems():
-        sql_fragment, new_params = _sql_condition(k,v)
+    for k, v in crit.iteritems():
+        sql_fragment, new_params = _sql_condition(k, v)
         sql.append(sql_fragment)
         params += new_params
 
     return " INTERSECT ".join(sql), params
+
 
 class DB(object):
     ANY = ANY
     Starts = Starts
 
     def __init__(self, path, sync=True):
-        self.conn = sqlite3.connect(path, timeout=60, isolation_level='DEFERRED')
-        self.conn.execute("PRAGMA synchronous = %s" % (sync and 'NORMAL' or 'OFF'))
+        self.conn = sqlite3.connect(
+            path, timeout=60, isolation_level='DEFERRED')
+        self.conn.execute("PRAGMA synchronous = %s" %
+                          (sync and 'NORMAL' or 'OFF'))
         self.cursor = self.conn.cursor()
         create_DB(self.conn)
         self.__idCache = dict()
@@ -88,11 +96,14 @@ class DB(object):
             return default
 
     def _get_id(self, tbl, id):
-        objid = self._query_single("SELECT %sid FROM %s WHERE %s = ?" % (tbl, tbl, tbl), (id,))
+        objid = self._query_single(
+            "SELECT %sid FROM %s WHERE %s = ?" % (tbl, tbl, tbl), (id,))
         if not objid:
             with self.lock:
-                self.cursor.execute("INSERT OR IGNORE INTO %s (%s) VALUES (?)" % (tbl, tbl), (id,))
-                objid = self._query_single("SELECT %sid FROM %s WHERE %s = ?" % (tbl, tbl, tbl), (id,))
+                self.cursor.execute(
+                    "INSERT OR IGNORE INTO %s (%s) VALUES (?)" % (tbl, tbl), (id,))
+                objid = self._query_single(
+                    "SELECT %sid FROM %s WHERE %s = ?" % (tbl, tbl, tbl), (id,))
         return objid
 
     def _getCachedId(self, tbl, id):
@@ -106,15 +117,18 @@ class DB(object):
     def get(self, obj, fields=None):
         if isinstance(obj, int):
             objid = obj
-            obj = self._query_single('SELECT obj FROM obj WHERE objid = ?', (objid,))
+            obj = self._query_single(
+                'SELECT obj FROM obj WHERE objid = ?', (objid,))
         else:
-            objid = self._query_single('SELECT objid FROM obj WHERE obj = ?', (obj,))
+            objid = self._query_single(
+                'SELECT objid FROM obj WHERE obj = ?', (obj,))
         obj = Object(obj)
         query = "SELECT key, timestamp, listid FROM map NATURAL JOIN key WHERE objid = ?"
         if fields is not None:
             query += " AND key IN (" + ', '.join(('?',) * len(fields)) + ")"
-        for key, timestamp, listid in self._query_all(query, (objid,)+tuple(fields or ())):
-            values = ValueSet(v=(x for x, in self._query_all("SELECT value FROM list WHERE listid = ?", (listid,))), t=timestamp)
+        for key, timestamp, listid in self._query_all(query, (objid,) + tuple(fields or ())):
+            values = ValueSet(v=(x for x, in self._query_all(
+                "SELECT value FROM list WHERE listid = ?", (listid,))), t=timestamp)
             obj._dict[key] = values
         return obj
 
@@ -122,33 +136,41 @@ class DB(object):
         return self.get(obj)
 
     def __delitem__(self, obj):
+
         objid = self._get_id('obj', obj.id)
         t = time()
-        self.conn.execute("""INSERT OR REPLACE INTO map (objid, keyid, timestamp, listid)
+        with self.lock:
+            self.conn.execute("""INSERT OR REPLACE INTO map (objid, keyid, timestamp, listid)
                             SELECT objid, keyid, ?, NULL FROM map WHERE objid = ?""", (t, objid))
 
     def update_attr(self, objid, key, values):
         objid = self._get_id('obj', objid)
         keyid = self._getCachedId('key', key)
-        tstamp = self._query_single("SELECT timestamp FROM map WHERE objid = ? AND keyid = ?", (objid, keyid))
+        tstamp = self._query_single(
+            "SELECT timestamp FROM map WHERE objid = ? AND keyid = ?", (objid, keyid))
         if values.t > tstamp:
             newlistid = self._insert_list(values)
-            cursor = self.conn.cursor()
-            cursor.execute("""INSERT OR REPLACE INTO map (objid, keyid, timestamp, listid)
+            with self.lock:
+                cursor = self.conn.cursor()
+                cursor.execute("""INSERT OR REPLACE INTO map (objid, keyid, timestamp, listid)
                             VALUES (?, ?, ?, ?)""",
-                            (objid, keyid, values.t, newlistid))
+                               (objid, keyid, values.t, newlistid))
             return cursor.lastrowid
         else:
             return False
 
     def _get_list_id(self, values):
         _values = set(values)
-        suspects = [x for (x,) in self._query_all("SELECT listid FROM list WHERE value = ?", (_values.pop(),) )]
+        suspects = [x for (x,) in self._query_all(
+            "SELECT listid FROM list WHERE value = ?", (_values.pop(),))]
         while _values and suspects:
-            query = "SELECT listid FROM list WHERE value = ? AND listid IN (%s)" % ( ','.join(['?']*len(suspects)) )
-            suspects = [x for (x,) in self._query_all(query, [_values.pop()] + suspects)]
+            query = "SELECT listid FROM list WHERE value = ? AND listid IN (%s)" % (
+                ','.join(['?'] * len(suspects)))
+            suspects = [
+                x for (x,) in self._query_all(query, [_values.pop()] + suspects)]
         if suspects and (not _values):
-            query = "SELECT listid FROM list WHERE listid IN (%s) GROUP BY listid HAVING COUNT(value) = ?" % ( ','.join(['?']*len(suspects)) )
+            query = "SELECT listid FROM list WHERE listid IN (%s) GROUP BY listid HAVING COUNT(value) = ?" % (
+                ','.join(['?'] * len(suspects)))
             listid = self._query_single(query, suspects + [len(values)])
             if listid:
                 return listid
@@ -159,9 +181,11 @@ class DB(object):
         listid = self._get_list_id(values)
         if listid:
             return listid
-        newlistid = (self._query_single("SELECT MAX(listid) FROM list") or 0) + 1
+        newlistid = (
+            self._query_single("SELECT MAX(listid) FROM list") or 0) + 1
         with self.lock:
-            self.conn.cursor().executemany("INSERT INTO list (listid, value) VALUES (?, ?)", [(newlistid, value) for value in values])
+            self.conn.cursor().executemany("INSERT INTO list (listid, value) VALUES (?, ?)",
+                                           [(newlistid, value) for value in values])
         return newlistid
 
     def query_ids(self, criteria):
@@ -188,12 +212,13 @@ class DB(object):
             for key in obj._dirty:
                 values = _dict[key]
                 keyid = self._getCachedId('key', key)
-                old_timestamp = self._query_single("SELECT timestamp FROM map WHERE objid = ? and keyid = ?", (objid, keyid))
+                old_timestamp = self._query_single(
+                    "SELECT timestamp FROM map WHERE objid = ? and keyid = ?", (objid, keyid))
                 if not old_timestamp or values.t > old_timestamp:
                     listid = self._insert_list(values)
                     cursor.execute("""INSERT OR REPLACE INTO map (objid, keyid, timestamp, listid)
                                         VALUES (?, ?, ?, ?)""",
-                                        (objid, keyid, _dict[key].t, listid))
+                                   (objid, keyid, _dict[key].t, listid))
         obj._dirty.clear()
 
     def commit(self):
@@ -203,7 +228,7 @@ class DB(object):
     def vacuum(self, delete_grace=DEFAULT_GRACE):
         with self.lock:
             if delete_grace is not None:
-                self.conn.execute("DELETE FROM map WHERE timestamp < ? AND listid IS NULL", (time()-delete_grace,))
+                self.conn.execute("DELETE FROM map WHERE timestamp < ? AND listid IS NULL", (time() - delete_grace,))
             for x, in self._query_all("SELECT DISTINCT list.listid FROM list LEFT JOIN map ON (map.listid = list.listid) WHERE map.listid IS NULL", ()):
                 self.conn.execute("DELETE FROM list WHERE listid = ?", (x,))
             for x, in self._query_all("SELECT DISTINCT key.keyid FROM key LEFT JOIN map ON (key.keyid = map.keyid) WHERE map.keyid IS NULL", ()):
@@ -214,11 +239,12 @@ class DB(object):
 
     def get_public_mappings_after(self, serial=0, limit=1024):
         for obj, key, tstamp, serial, listid in self._query_all("SELECT obj, key, timestamp, serial, listid FROM map NATURAL JOIN key NATURAL JOIN obj WHERE serial > ? AND NOT key LIKE '@%' ORDER BY serial LIMIT ?", (serial, limit)):
-            values = set(x for x, in self._query_all("SELECT value FROM list WHERE listid = ?", (listid,)))
+            values = set(x for x, in self._query_all(
+                "SELECT value FROM list WHERE listid = ?", (listid,)))
             yield obj, key, tstamp, serial, values
 
     def last_serial(self):
-        return self._query_single("SELECT MAX(serial) FROM map") or 0;
+        return self._query_single("SELECT MAX(serial) FROM map") or 0
 
     def get_sync_state(self, peername):
         return {
@@ -227,4 +253,5 @@ class DB(object):
 
     def set_sync_state(self, peername, last_received):
         with self.lock:
-            self.conn.cursor().execute("INSERT OR REPLACE INTO sync_state (peername, last_received) VALUES (?, ?)", (peername, last_received))
+            self.conn.cursor().execute(
+                "INSERT OR REPLACE INTO sync_state (peername, last_received) VALUES (?, ?)", (peername, last_received))
