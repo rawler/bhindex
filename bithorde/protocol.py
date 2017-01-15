b70f0f2279b5965567e921cf1c8cec428643da9e
Ulrik Mikaelsson <ulrik.mikaelsson@gmail.com>
2017-01-15 23:38:08 +0100

    bithorde/protocol: Cleanup and decouple encodeMessage from protobuf.internal

--- bithorde/protocol.py
+++ bithorde/protocol.py
@@ -3,49 +3,55 @@
 Helpers to code/decode the bithorde framing protocol
 '''
 
-import socket
-import os, os.path
-from base64 import b32decode as _b32decode
-from cStringIO import StringIO
-
 import bithorde_pb2 as message
 
-from google.protobuf import descriptor
-from google.protobuf.internal import encoder, decoder
+from google.protobuf.internal import decoder
 
 # Protocol imports and definitions
 MSG_MAP = message.Stream.DESCRIPTOR.fields_by_number
 MSG_TYPE_MAP = {
-    message.HandShake:     1,
-    message.BindRead:      2,
-    message.AssetStatus:   3,
-    message.Read.Request:  5,
+    message.HandShake: 1,
+    message.BindRead: 2,
+    message.AssetStatus: 3,
+    message.Read.Request: 5,
     message.Read.Response: 6,
-    message.BindWrite:     7,
-    message.DataSegment:   8,
+    message.BindWrite: 7,
+    message.DataSegment: 8,
     message.HandShakeConfirmed: 9,
     message.Ping: 10,
 }
+
 
 def decodeMessage(buf, msg_map=MSG_MAP):
     '''Decodes a single message from buffer
     @return (msg, bytesConsumed)
     @raises IndexError if buffer did not contain complete message
     '''
-    id, newpos = decoder._DecodeVarint32(buf,0)
-    size, newpos = decoder._DecodeVarint32(buf,newpos)
+    id, newpos = decoder._DecodeVarint32(buf, 0)
+    size, newpos = decoder._DecodeVarint32(buf, newpos)
     id = id >> 3
-    msgend = newpos+size
+    msgend = newpos + size
     if msgend > len(buf):
-        raise IndexError, 'Incomplete message'
+        raise IndexError('Incomplete message')
     msg = msg_map[id].message_type._concrete_class()
     msg.ParseFromString(buf[newpos:msgend])
     return msg, msgend
 
+
+def encode_varint(arr, i):
+    if i > 127:
+        arr.append((i & 127) | 128)
+        encode_varint(arr, i >> 7)
+    else:
+        arr.append(i)
+
+
 def encodeMessage(msg, msgtype=None, msg_map=MSG_TYPE_MAP):
     if not msgtype:
         msgtype = msg_map[type(msg)]
-    buf = StringIO()
-    enc = encoder.MessageEncoder(msgtype, False, False)
-    enc(buf.write, msg)
-    return buf.getvalue()
+    msg = msg.SerializePartialToString()
+
+    hdr = []
+    encode_varint(hdr, msgtype << 3 | 2)
+    encode_varint(hdr, len(msg))
+    return bytearray(hdr) + msg
