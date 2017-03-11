6b61ae45e4f52b92446c6b8b3088417b02aa384c
Ulrik Mikaelsson <ulrik.mikaelsson@gmail.com>
2017-03-12 00:43:56 +0100

    Update _pb2-modules

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['protobuf', 'eventlet'],
+    install_requires=['protobuf>=2.6,<3', 'eventlet'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
